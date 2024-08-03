21f708a13c6483247f6a1437ec81fe39b90d8a6b
Lei <18039026206lei@gmail.com>
2024-08-03 20:19:01 +0800

    Second submit: finish adding

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -2,9 +2,20 @@ import React from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
+import { useState } from "react";
+
 
 export default function App(props) {
-  const taskList = props.tasks.map((task) => (
+
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = { id: "id", name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  
+
+  const taskList = tasks?.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
@@ -16,14 +27,14 @@ export default function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
-      
+      <Form onSubmit={addTask} />
+
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
-        
+
       <h2 id="list-heading">3 tasks remaining</h2>
       <ul
         role="list"
